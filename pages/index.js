6bb2aff73dce5f4bec223a35500df7ff52f443fc
Kristof Hayes <kristof@enano.io>
2022-07-25 22:11:06 +0100

    google gtag.js

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,25 @@ export default function Home() {
             redirect_uri: 'https://example.com/callback'
           }); */}
         </script>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       </Head>
       <main>
         <h1 className="title">
